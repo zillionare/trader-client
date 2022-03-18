1838e1818336be83abaa725feae0a15b0053c7ce
Henry Tao <henrytao@263.net>
2022-03-18 17:43:54 +0800

    🦄 refactor: reorg source files

--- traderclient/transport.py
+++ traderclient/transport.py
@@ -3,34 +3,9 @@ import uuid
 
 import requests
 
+from traderclient.utils import get_cmd, status_ok
+
 logger = logging.getLogger(__name__)
-
-
-def status_ok(code):
-    return code in [200, 201, 204]
-
-
-def get_cmd(url):
-    cmd = url.split("/")[-1]
-    return {
-        "info": "获取账户信息",
-        "balance": "获取账户余额",
-        "available_money": "获取可用资金",
-        "position": "获取持仓信息",
-        "buy": "买入股票",
-        "sell": "卖出股票",
-        "available_shares": "获取可用持仓",
-        "today_entrusts": "获取当日委托",
-        "today_trades": "获取当日成交",
-        "cancel_entrust": "撤单",
-        "cancel_all_entrust": "全部撤单",
-        "market_buy": "市价买入",
-        "market_sell": "市价卖出",
-        "sell_percent": "按比例卖出股票",
-        "sell_all": "全部卖出",
-        "get_trades_in_range": "获取指定时间段的成交记录",
-        "get_entrusts_in_range": "获取指定时间段的委托记录",
-    }.get(cmd, "未知命令")
 
 
 def get_result(rsp, cmd: str = None):
--- traderclient/utils.py
+++ traderclient/utils.py
@@ -1,21 +1,30 @@
-import logging
+# -*- coding: utf-8 -*-
+# @Author   : henry
+# @Time     : 2022-03-09 15:08
 
 
-def enable_logging(level: str = "info"):
-    logger = logging.getLogger()
+def status_ok(code: int):
+    return code in [200, 201, 204]
 
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(message)s")
-    handler.setFormatter(formatter)
 
-    logger.addHandler(handler)
-
-    level = {
-        "debug": logging.DEBUG,
-        "info": logging.INFO,
-        "warning": logging.WARNING,
-        "error": logging.ERROR,
-        "critical": logging.CRITICAL,
-    }.get(level.lower(), logging.INFO)
-
-    logger.setLevel(level)
+def get_cmd(url: str):
+    cmd = url.split("/")[-1]
+    return {
+        "info": "获取账户信息",
+        "balance": "获取账户余额",
+        "available_money": "获取可用资金",
+        "position": "获取持仓信息",
+        "buy": "买入股票",
+        "sell": "卖出股票",
+        "available_shares": "获取可用持仓",
+        "today_entrusts": "获取当日委托",
+        "today_trades": "获取当日成交",
+        "cancel_entrust": "撤单",
+        "cancel_all_entrust": "全部撤单",
+        "market_buy": "市价买入",
+        "market_sell": "市价卖出",
+        "sell_percent": "按比例卖出股票",
+        "sell_all": "全部卖出",
+        "get_trades_in_range": "获取指定时间段的成交记录",
+        "get_entrusts_in_range": "获取指定时间段的委托记录",
+    }.get(cmd, "未知命令")
